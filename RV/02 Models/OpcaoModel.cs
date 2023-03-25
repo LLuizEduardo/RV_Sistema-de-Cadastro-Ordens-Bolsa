e9ef62704c436eb65f1584a71a4cc3d4bae84836
Luiz Eduardo Fernandes Junior <luiz.eduardo@aec.com.br>
2023-03-25 19:30:11 -0300

    fix: validacoes e importacoes Models

--- RV/02 Models/OpcaoModel.cs
+++ RV/02 Models/OpcaoModel.cs
@@ -1,20 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace RV
 {
     public class OpcaoModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite a data")]
         public DateTime Data { get; set; }
+
+        [Required(ErrorMessage = "Digite a Opção")]
         public string Opcao { get; set; }
         public string Tipo { get; set; }
         public string Papel { get; set; }
+
+        [Required(ErrorMessage = "Digite o Strike")]
         public double Strike { get; set; }
+
+        [Required(ErrorMessage = "Digite a quantidade")]
         public int Quantidade { get; set; }
         public DateTime Vencimento { get; set; }
+
+        [Required(ErrorMessage = "Digite o valor")]
         public double Valor { get; set; }
         public string Ordem { get; set; }
         public string Corretora { get; set; }
