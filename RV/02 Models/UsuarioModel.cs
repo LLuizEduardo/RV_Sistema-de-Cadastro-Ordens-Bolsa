9fc66c0ef34e52ba36d27a7e5cc6887a79a17e54
Luiz Eduardo Fernandes Junior <luiz.eduardo@aec.com.br>
2023-04-13 21:19:06 -0300

    login page

---
+++ RV/02 Models/UsuarioModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RV
+{
+    public class UsuarioModel
+    {
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Senha { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAlteracao { get; set; }
+
+    }
+}
