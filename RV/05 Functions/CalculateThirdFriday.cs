673533df95e07ed34c398aa44fa1363b14342f24
Luiz Eduardo Fernandes Junior <luiz.eduardo@aec.com.br>
2023-04-25 13:00:13 -0300

    added 'opcoes' functions

---
+++ RV/05 Functions/CalculateThirdFriday.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RV
+{
+    public class Calendar
+    {
+        public DateTime CalculateThirdFriday(DateTime date)
+        {
+            DateTime thirdFriday = new(date.Year, date.Month, 15);
+
+            while (thirdFriday.DayOfWeek != DayOfWeek.Friday)
+            {
+                thirdFriday = thirdFriday.AddDays(1);
+            }
+
+            return thirdFriday;
+        }
+    }
+}
---
+++ RV/05 Functions/TipoOrdem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace RV
+{
+    public class TipoOrdem
+    {
+        public Dictionary<char, string> DicionarioOpcoes()
+        {
+            int i = 1;
+            int mes;
+            string tipo;
+
+            Dictionary<char, string> dic = new();
+            for (char letter = 'A'; letter <= 'X'; letter++)
+            {
+                mes = (i > 12) ? i - 12 : i;
+                tipo = (i > 12) ? "Put" : "Call";
+                dic.Add(letter, mes + "," + tipo);
+                i++;
+            };
+
+            return dic;
+        }
+    }
+}
