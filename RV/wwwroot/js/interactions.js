70e3b15247c57c56592b0579b8a3b323c8866d21
Luiz Eduardo Fernandes Junior <luiz.eduardo@aec.com.br>
2023-05-02 19:06:49 -0300

    added interactions.js

---
+++ RV/wwwroot/js/interactions.js
@@ -0,0 +1,11 @@
+﻿
+
+function exibir(pos, botao) {
+    const hy = document.getElementsByTagName('td');
+
+    for (var i = pos; i < hy.length; i = i + pos + 1) {
+
+        document.getElementsByTagName('td')[i].style.display = "block";
+    }
+    document.getElementById(botao).style.display = "block";
+}
