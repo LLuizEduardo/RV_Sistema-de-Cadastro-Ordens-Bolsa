e20c2315545c29bb9499a047c76c891c7c7fa518
Luiz Eduardo Fernandes Junior <luiz.eduardo@aec.com.br>
2023-07-04 19:15:52 -0300

    Migrations - User added into 'Acoes and 'Opcoes

--- infrastructure/Migrations/20230627232559_inclusao_usuario_tb_acoes_e_opcoes.Designer.cs
+++ infrastructure/Migrations/20230627232559_inclusao_usuario_tb_acoes_e_opcoes.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RV.Infrastucture.Data;
 
 namespace RV.Infrastucture.Migrations
 {
     [DbContext(typeof(BancoContent))]
-    partial class BancoContentModelSnapshot : ModelSnapshot
+    [Migration("20230627232559_inclusao_usuario_tb_acoes_e_opcoes")]
+    partial class inclusao_usuario_tb_acoes_e_opcoes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ infrastructure/Migrations/20230627232559_inclusao_usuario_tb_acoes_e_opcoes.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RV.Infrastucture.Migrations
+{
+    public partial class inclusao_usuario_tb_acoes_e_opcoes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UsuarioId",
+                table: "Opcoes",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsuarioId",
+                table: "Acoes",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Opcoes_UsuarioId",
+                table: "Opcoes",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Acoes_UsuarioId",
+                table: "Acoes",
+                column: "UsuarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Acoes_Usuarios_UsuarioId",
+                table: "Acoes",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Opcoes_Usuarios_UsuarioId",
+                table: "Opcoes",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Acoes_Usuarios_UsuarioId",
+                table: "Acoes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Opcoes_Usuarios_UsuarioId",
+                table: "Opcoes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Opcoes_UsuarioId",
+                table: "Opcoes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Acoes_UsuarioId",
+                table: "Acoes");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Opcoes");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Acoes");
+        }
+    }
+}
